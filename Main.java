9d7eb4cb9200026c2253cdfafc9fcd251edd13a2
torbuntu <sorensentor@tuta.io>
2021-06-03 16:25:54 -0500

    overhaul movement. Add some sprites

--- Main.java
+++ Main.java
@@ -6,17 +6,25 @@ import femto.palette.Castpixel16;
 import femto.font.TIC80;
 
 class Main extends State {
-
+    
     HiRes16Color screen; // the screenmode we want to draw with
 
     Robo robo;
     Trash trash;
+    Charger charger;
+    // 0 - cheese, 1 - chips, etc...
+    int trashID;
     
     int vx, vy, battery, fill, tic;
     
+    // Coordinate system for Robot - r and Trash - t
+    int rx, ry, tx, ty, nextX, nextY;
+    
     // 0 - left, 1 - up, 2 - right, 3 - down
     int direction, nudge;
-    boolean charging, primeChange, full;
+    boolean primeChange;
+    
+    GameState state;
 
     public static void main(String[] args){
         Game.run( TIC80.font(), new Main() );
@@ -24,6 +32,8 @@ class Main extends State {
     
     void init(){
         screen = new HiRes16Color(Castpixel16.palette(), TIC80.font());
+        charger = new Charger();
+        charger.charging();
         
         robo = new Robo();
         robo.hor();
@@ -41,221 +51,246 @@ class Main extends State {
         vy = 0;
         
         direction = 2;
-        charging = false;
         primeChange = false;
+        
+        rx = 0;
+        ry = 0;
+        
+        nextX = 1;
+        nextY = 0;
+        
+        state = GameState.RUNNING;
 
     }
-    
 
     void shutdown(){
         screen = null;
     }
     
+    
     void update(){
-        //if( Button.A.justPressed() ) Game.changeState( new Main() );
+        // For testing
+        if( Button.A.justPressed() ) Game.changeState( new Main() );
         screen.clear(5);
         
+        
+        switch(state){
+            case GameState.FULL:
+                if(Button.A.justPressed() || Button.B.justPressed()){
+                    fill-=10;
+                    if(fill <= 0){
+                        fill = 0;
+                        state = GameState.RUNNING;
+                    }
+                }
+                break;
+            case GameState.CHARGING:
+                if(Button.Left.justPressed() || Button.Right.justPressed() ||
+                    Button.Up.justPressed() || Button.Down.justPressed()){
+                    battery+=10;
+                }
+                
+                if(battery >= 100){
+                    battery = 100;
+                    state = GameState.RUNNING;
+                }
+                break;
+            case GameState.RUNNING:
+                if(tic > 0)tic--;
+                else{
+                    battery--;
+                    tic = 15;
+                }
+                
+                if(battery <= 0){
+                    switchToCharging();
+                    return;
+                }
+                
+                if(Button.Left.justPressed()){
+                    nudge = 0;
+                    primeChange = true;
+                } 
+                if(Button.Right.justPressed()){
+                    nudge = 2;
+                    primeChange = true;
+                }
+                if(Button.Up.justPressed()){
+                    nudge = 1;
+                    primeChange = true;
+                }
+                if(Button.Down.justPressed() ){
+                    nudge = 3;
+                    primeChange = true;
+                }
+        
+                makeMove();
+                break;
+        }
+        
+        //debugging grid
         for(int i = 0; i < 12; i++){
             for(int j = 0; j < 8; j++){
                 screen.drawRect(14+i*16, 32+j*16, 16, 16, 9, true);    
             }
         }
-        screen.fillRect(14, 32, 16, 16, 2, true);
+
         //draw HUD box
         screen.fillRect(0, 0, 240, 16, 11, true);
         screen.fillRect(0, 24, battery, 3, 7, false);
         screen.fillRect(0, 22, fill, 3, 9, false);
         screen.setTextPosition(0, 0);
         screen.setTextColor(0);
-        screen.println(robo.x);
-        screen.println(robo.y);
+        screen.println(rx);
+        screen.println(ry);
         screen.setTextPosition(40, 0);
-        screen.print(trash.x);
+        screen.print(tx);
         screen.setTextPosition(40, 8);
-        screen.print(trash.y);
+        screen.print(ty);
         
         if(primeChange){
             screen.setTextPosition(100, 0);
             screen.print("<-- NUDGE -->");
         }
-
         
+        charger.draw(screen, 14, 32);
         trash.draw(screen);
         robo.draw(screen);
         
         screen.flush();
-        if(full){
-            if(Button.A.justPressed() || Button.B.justPressed()){
-                fill-=10;
-                if(fill <= 0){
-                    fill = 0;
-                    full = false;
-                }
-            }
-            return;
-        }
-        if(charging){
-            if(Button.Left.justPressed() || Button.Right.justPressed() ||
-                Button.Up.justPressed() || Button.Down.justPressed()){
-                battery+=10;
-            }
-            
-            if(battery >= 100){
-                battery = 100;
-                charging = false;
-            }
-            return;
-        }
-        
-        if(tic > 0)tic--;
-        else{
-            battery--;
-            tic = 15;
-        }
-        
-        if(battery <= 0){
-            robo.x = 14;
-            robo.y = 32;
-            charging = true;
-            robo.setMirrored(false);
-            robo.setFlipped(false);
-            robo.hor();
-            direction = 2;
-            vx = 16;
-            return;
-        }
-        
-        if(Button.Left.justPressed()){
-            nudge = 0;
-            primeChange = true;
-        } 
-        if(Button.Right.justPressed()){
-            nudge = 2;
-            primeChange = true;
-        }
-        if(Button.Up.justPressed()){
-            nudge = 1;
-            primeChange = true;
-        }
-        if(Button.Down.justPressed() ){
-            nudge = 3;
-            primeChange = true;
-        }
-
-        // movement
-        if(primeChange && vx == 0 && vy == 0){
-            primeChange = false;
-            direction = nudge;
-            switch(nudge){
-                case 0:
-                    tryLeft();
-                    break;
-                case 2:
-                    tryRight();
-                    break;
-                case 1:
-                    tryUp();
-                    break;
-                case 3:
-                    tryDown();
-                    break;
-            }
-
-            return;
-        }
-        if(vx != 0){
-            if(vx > 0){
-                vx--;
-                robo.x += 1;
-            } else {
+    }
+    
+    void switchToCharging(){
+        robo.x = 14;
+        robo.y = 32;
+        rx = 0;
+        ry = 0;
+        nextX = 1;
+        nextY = 0;
+        robo.setMirrored(false);
+        robo.setFlipped(false);
+        robo.hor();
+        direction = 2;
+        vx = 16;
+        state = GameState.CHARGING;
+    }
+    
+    void makeMove(){
+        switch(direction){
+            case 0: 
                 vx++;
-                robo.x -= 1;
-            }
-        }else if(vy != 0){
-            if(vy > 0){
-                vy--;
-                robo.y += 1;
-            } else {
+                robo.x-=1;
+                if(vx == 0){
+                    rx = nextX;
+                    if(primeChange){
+                        moveNudge();
+                    }else{
+                        tryLeft();
+                    }
+                }
+                break;
+            case 2:
+                vx--;
+                robo.x+=1;
+                if(vx == 0){
+                    rx = nextX;
+                    if(primeChange){
+                        moveNudge();
+                    }else{
+                        tryRight();
+                    }
+                }
+                break;
+            case 1:
                 vy++;
-                robo.y -= 1;
-            }
-        }else{
-            checkTrash();
-            switch(direction){
-                case 0: 
-                    if(robo.x > 14){
-                        vx = -16;
+                robo.y-=1;
+                if(vy == 0){
+                    ry = nextY;
+                    if(primeChange){
+                        moveNudge();
                     }else{
-                        robo.x = 14;
-                        changeDirection();
+                        tryUp();
                     }
-                    
-                    break;
-                case 2:
-                    if(robo.x < 14+11*16){
-                        vx = 16;
+                }
+                break;
+            case 3:
+                vy--;
+                robo.y+=1;
+                if(vy == 0){
+                    ry = nextY;
+                    if(primeChange){
+                        moveNudge();
                     }else{
-                        robo.x = 14+11*16;
-                        changeDirection();
+                        tryDown();
                     }
-                    break;
-                case 1:
-                    if( robo.y > 32 ) {
-                        vy = -16;
-                    }else{
-                        robo.y = 32;
-                        changeDirection();
-                    }
-                    break;
-                case 3:
-                    if( robo.y < 32+7*16 ){
-                        vy = 16;
-                    }else{
-                        robo.y = 32+7*16;
-                        changeDirection();
-                    }
-                    break;
-            }
+                }
+                break;
         }
-        
+    }
+    
+    void moveNudge(){
+        primeChange = false;
+        direction = nudge;
+        switch(direction){
+            case 0:
+                tryLeft();
+                break;
+            case 2:
+                tryRight();
+                break;
+            case 1:
+                tryUp();
+                break;
+            case 3:
+                tryDown();
+                break;
+        }
     }
     void tryLeft(){
-        if( robo.x > 14 ) {
+        checkTrash();
+        if( rx > 0 ) {
             robo.hor();
             robo.setMirrored(true);
             robo.setFlipped(false);
             vx = -16;
+            nextX--;
         } else{
             changeDirection();
         }
     }
     
     void tryRight(){
-        if( robo.x < 14+11*16 ){
+        checkTrash();
+        if( rx < 11 ){
             robo.hor();
             robo.setMirrored(false);
             robo.setFlipped(false);
             vx = 16;
+            nextX++;
         }else{
             changeDirection();
         }
     }
     
     void tryUp(){
-        if(robo.y > 32 ){
+        checkTrash();
+        if(ry > 0 ){
             robo.ver();
             robo.setFlipped(false);
             vy = -16;
+            nextY--;
         }else{
             changeDirection();
         }
     }
     
     void tryDown(){
-        if( robo.y < 32+7*16 ){
+        checkTrash();
+        if( ry < 7){
             robo.ver();
             robo.setFlipped(true);
             vy = 16;
+            nextY++;
         }else{
             changeDirection();
         }
@@ -290,30 +325,49 @@ class Main extends State {
         if(x == 0 && y == 0){
             x++;
         }
+        if(x == 11 && y == 7){
+            x--;
+        }
+        tx = x;
+        ty = y;
+        if(rx == tx && ry == ty){
+            setTrash();
+        }
 
         x = 14+x*16;
         y = 32+y*16;
         
-        if(Math.floor(robo.x) == Math.floor(x) && Math.floor(robo.y) == Math.floor(y)){
-            setTrash();
-        }
-        
         trash.setPosition(x, y);
-        trash.cheese();
+        // TODO: increase for different trash types
+        trashID = Math.random(0, 2);
+        switch(trashID){
+            case 0:
+                trash.cheese();
+                break;
+            case 1:
+                trash.chips();
+                break;
+        }
     }
     
     void checkTrash(){
-        if(Math.floor(robo.x) == Math.floor(trash.x) && Math.floor(robo.y) == Math.floor(trash.y)){
+        if(rx == tx && ry == ty){
             setTrash();
             fill+=10;
             if(fill > 200){
-                full = true;
                 robo.hor();
                 robo.setMirrored(true);
                 robo.setPosition(14+11*16, 32+7*16);
-                vx=-16;
+                rx = 11;
+                ry = 7;
+                vx =-16;
                 direction = 0;
+                state = GameState.FULL;
             }
         }
     }
+}
+
+enum GameState {
+    FULL, CHARGING, RUNNING, PAUSE
 }
